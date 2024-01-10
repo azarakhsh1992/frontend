477524d8866e9750176a7f93fe4df439031a247a
abaspoor <abaspoor>
2024-01-10 20:49:34 +0330

    request completed

--- frontend2/src/components/page1/section-top.js
+++ frontend2/src/components/page1/section-top.js
@@ -40,20 +40,18 @@ const SectionTop = ({QrScanned,RequestChecked,SetRequestChecked,setQr,DoorQr,Aut
                                 <Typography className={clsx(classes.btn_typo)}>Cabinet Overview</Typography>
                             </Button>
 
-                            <Button variant={"contained"} className={clsx(classes.btn,classes.btn_logout)}>
-                                <Typography className={clsx(classes.btn_typo)}>Logout</Typography>
+                            <Button variant={"contained"} className={clsx(classes.btn)}>
+                                <Typography className={clsx(classes.btn_typo)}>Cabinet XY Hall</Typography>
                             </Button>
                         </Box>
                         <Box className={clsx(classes.rightbox)}>
                             <Button variant={"contained"} className={clsx(classes.btn)}>
-                                <Typography className={clsx(classes.btn_typo)}>Cabinet XY Hall</Typography>
-                            </Button>
-
-
-                            <Button variant={"contained"} className={clsx(classes.btn)}>
                                 <Typography className={clsx(classes.btn_typo)}>My Cabinets</Typography>
                             </Button>
 
+                            <Button variant={"contained"} className={clsx(classes.btn,classes.btn_logout)}>
+                                <Typography className={clsx(classes.btn_typo)}>Logout</Typography>
+                            </Button>
                             {/*<Button variant={"contained"} className={clsx(classes.btn)} onClick={(event)=> {handleButton(event)}}*/}
                             {/*	disabled={QrScanned}>*/}
                             {/*    <Typography className={clsx(classes.btn_typo)}>Zugang anfordem</Typography>*/}
