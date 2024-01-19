765e0b31ace6656fa45f20ff46be59214fe6f195
abaspoor <abaspoor>
2024-01-19 22:29:13 +0330

    request completed

--- frontend2/src/theme/Theme.js
+++ frontend2/src/theme/Theme.js
@@ -42,13 +42,13 @@ export const theme = createTheme({
     },
     typography: {
         fontFamily: [
+            '"Helvetica Neue"',
+            'Arial',
+            'sans-serif',
             '-apple-system',
             'BlinkMacSystemFont',
             '"Segoe UI"',
             'Roboto',
-            '"Helvetica Neue"',
-            'Arial',
-            'sans-serif',
             '"Apple Color Emoji"',
             '"Segoe UI Emoji"',
             '"Segoe UI Symbol"',
