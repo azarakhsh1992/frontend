abc534ce5481f78ad6203fb59c645fc17f960bd6
abaspoor <abaspoor>
2024-01-19 21:54:38 +0330

    request completed

--- frontend2/src/components/page1/section-middle-components/section-door.js
+++ frontend2/src/components/page1/section-middle-components/section-door.js
@@ -13,15 +13,22 @@ import PopupDataDoor from "./popup-data-door";
 
 export const SectionDoor = ({DoorDatas,historydoor}) => {
 
+    const DoorColor = {
+        'Open':"red",
+        'Locked':"green",
+        'Unlocked':"orange",
+        'Faulted':"yellow",
+    }
     const classes = useStyles();
     return (
         <Container className={clsx(classes.maincontainer)}>
             <Box className={clsx(classes.mainbox)}>
                 <Box className={clsx(classes.door_box)}>
                     <PopupDataDoor txt={"history"} HistoryData={(historydoor && historydoor)}/>
-                    <Typography className={clsx(classes.door_status_typo)} sx={{backgroundColor:`${(DoorDatas["Current"]==="closed") ? "red":"green"} !important`}}>
+                    {DoorDatas["Current"] && <Typography className={clsx(classes.door_status_typo)}
+                                 sx={{backgroundColor: `${DoorColor[DoorDatas["Current"]]} !important`}}>
                         {DoorDatas["Current"]}
-                    </Typography>
+                    </Typography>}
                 </Box>
             </Box>
         </Container>
